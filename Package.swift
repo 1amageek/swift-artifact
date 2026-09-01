7477c2653dff528ded1101ec07076e4751082233
1amageek <tmy0x3@icloud.com>
2026-09-01 19:07:23 +0900

    Update MarkdownUI dependency to 0.2.1

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .library(name: "ArtifactWebRenderer", targets: ["ArtifactWebRenderer"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/1amageek/swift-markdown-ui.git", from: "0.2.0"),
+        .package(url: "https://github.com/1amageek/swift-markdown-ui.git", from: "0.2.1"),
         .package(url: "https://github.com/lukilabs/beautiful-mermaid-swift.git", from: "1.0.4"),
         .package(url: "https://github.com/1amageek/swift-knowledge-graph.git", exact: "0.3.0"),
         .package(url: "https://github.com/1amageek/CodeEditSourceEditor.git", from: "0.16.1"),
